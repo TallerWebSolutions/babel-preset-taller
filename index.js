c19e57e3566934612156ec5393f2b29fdf2f58a3
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-11-19 16:44:27 -0200

    FIxed legacy decorators plugin usage.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const presetEs2015 = require('babel-preset-es2015')
 const presetStage0 = require('babel-preset-stage-0')
 
-const transformDecoratorsLegacy = require('babel-plugin-transform-decorators-legacy')
+const transformDecoratorsLegacy = require('babel-plugin-transform-decorators-legacy').default
 const transformES3PropertyLiterals = require('babel-plugin-transform-es3-property-literals')
 const transformES3MemberExpressionLiterals = require('babel-plugin-transform-es3-member-expression-literals')
 const transformRuntime = require('babel-plugin-transform-runtime')
